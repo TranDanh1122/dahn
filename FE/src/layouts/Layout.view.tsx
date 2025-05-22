c53602e4da6401524c03dc93146f3a3cb47df816
TranDanh1122 <trandanh14042000@gmail.com>
2025-05-22 21:12:38 +0700

    add fallback layout

--- FE/src/layouts/Layout.view.tsx
+++ FE/src/layouts/Layout.view.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "./Header";
+import LoadingFallback from "@/components/LoadingFallback.component";
 
 export default React.memo(function Layout(): React.JSX.Element {
+    const location = useLocation()
     return <div className="w-screen h-screen bg-white relative overflow-hidden">
         <Header />
         <main className="w-full h-full flex  justify-center items-center">
-            <Outlet />
+            <React.Suspense fallback={<LoadingFallback/>} key={location.key}>   <Outlet /></React.Suspense>
+         
         </main>
     </div>
 })
