c96152721821c7f85c3e4bd6bfbe9921bfa24c78
Tran Thanh Danh <trandanh14042000@gmail.com>
2025-05-22 15:01:43 +0700

    change language auth social

--- FE/src/modules/auth/components/SocialAuth.tsx
+++ FE/src/modules/auth/components/SocialAuth.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { useAuthWSocial } from '@auth/flows/pkce/pkce.service'
 import Button from '@/components/Button.component'
-export default function SocialAuth(): React.JSX.Element {
+import { useTranslation } from 'react-i18next'
+export default React.memo(function SocialAuth(): React.JSX.Element {
     const socialAuth = useAuthWSocial()
     const handleGoogleAuth = () => socialAuth.action("google")
     const handleGitAuth = () => socialAuth.action("github")
+    const { t } = useTranslation('auth')
     return <div className=' space-y-3'>
-        <Button onClick={handleGoogleAuth} className="border border-neutral-200  w-full relative">
+        <Button onClick={handleGoogleAuth} className="border border-neutral-200  w-full relative line-clamp-1">
             <img src="/images/google.svg" alt="google logo" className="size-5 object-cover absolute top-1/2 left-2 -translate-y-1/2" />
-            Don’t want to text? Let Google help!
+            {t('google_auth')}
         </Button>
-        <Button onClick={handleGitAuth} className="border border-neutral-200  w-full relative">
+        <Button onClick={handleGitAuth} className="border border-neutral-200  w-full relative line-clamp-1">
             <img src="/images/github.svg" alt="github logo" className="size-5 object-cover absolute top-1/2 left-2 -translate-y-1/2" />
-            Want a dev's style? Use Github!
+            {t('github_auth')}
         </Button>
     </div>
-}
+})
