8b9d017e972bff7c4f3e29ead5a76ab17de630bd
Tran Thanh Danh <trandanh14042000@gmail.com>
2025-05-30 15:22:52 +0700

    fix build

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -4,7 +4,6 @@ import App from './App.tsx'
 import { store } from "@/stores"
 import { Provider } from "react-redux"
 import "@lang/index.ts"
-import { ErrorBoundary } from './ErrorBoudary.tsx'
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
     <App />
