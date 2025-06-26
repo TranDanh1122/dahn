309834a5e2e865a15272ea362d5fb626bf721e25
Tran Thanh Danh <trandanh14042000@gmail.com>
2025-06-26 15:31:36 +0700

    fix build

--- FE/src/modules/project/components/detail/components/main/steps/step3/Role/RoleDropContent.component.tsx
+++ FE/src/modules/project/components/detail/components/main/steps/step3/Role/RoleDropContent.component.tsx
@@ -1,5 +1,5 @@
 import type { RoleData } from "@project/models";
 import React from "react";
 export default function RoleDropContent({ role }: { role: RoleData }): React.JSX.Element {
-    return <></>
+    return <>{role.folder}</>
 }
